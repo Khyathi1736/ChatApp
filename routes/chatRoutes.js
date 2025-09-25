1249264c6c803ebb1c24b0ddd91d7e0399c25ac4
Khyathi <khyathivardhan17@gmail.com>
2025-09-25 14:19:10 +0530

    initial commit of chat routes

---
+++ routes/chatRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+// import {}
+
+const chatRoutes=express.Router();
+
+// All routes handled in webSocket.js file for realtime updates.
+
+
+
+
+
+
+
+
+
+export default chatRoutes;
